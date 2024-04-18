94c5fa3784af04e5703f7a2dd381bf61bfae3cdb
noe.merle <noe.merle@epitech.eu>
2024-04-18 09:16:00 +0200

    [add]: auth page (html and css)

---
+++ src/front-end/components/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        backgroundImage: theme => ({
+          'custom-gradient': 'linear-gradient(0deg, rgba(85,86,86,1) 18%, rgba(33,33,33,1) 34%, rgba(0,0,0,1) 59%, rgba(0,0,0,1) 100%)',
+        })
+      },
+    },
+  }
